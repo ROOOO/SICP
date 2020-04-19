65b7c837dc76c3c43c222e310a07e937190cd5bc
Liam Wang <ql.wang.gd@gmail.com>
2020-04-19 19:42:49 +0800

    Exercise 1.9

---
+++ 1-9.lisp
@@ -0,0 +1,35 @@
+(define (inc x)
+  (+ x 1))
+(define (dec x)
+  (- x 1))
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (inc (plus (dec a) b))))
+
+(trace plus)
+(plus 4 5)
+; (inc (plus 3 5))
+; (inc (inc (plus 2 5)))
+; (inc (inc (inc (plus 1 5))))
+; (inc (inc (inc (inc (plus 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (plus (dec a) (inc b))))
+
+(trace plus)
+(plus 4 5)
+; (plus 3 6)
+; (plus 2 7)
+; (plus 1 8)
+; (plus 0 9)
+; 9
+
