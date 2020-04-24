5768be3cdde571faaf8293a5d4f1585241a19bab
Liam Wang <ql.wang.gd@gmail.com>
2020-04-24 23:55:39 +0800

    Factorial. Iterative version.

---
+++ factorial.lisp
@@ -0,0 +1,8 @@
+(define (factorial n)
+  (factorial-iter 1 1 n))
+(define (factorial-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (factorial-iter (* product counter)
+                     (+ counter 1)
+                     max-count)))
